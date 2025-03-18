5f54c7c7a5588ad8cfafbf863374d5def287acf3
alessandraaguiluz <alessandraaguiluz0@gmail.com>
2025-03-18 15:47:11 -0600

    model rol y model seller status

---
+++ app/Models/SellerStatus.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SellerStatus extends Model
+{
+    use HasFactory;
+
+    protected $table = 'seller_status';
+
+    protected $fillable = [
+        'seller_status',
+    ];
+}
