c51a302d6edeae423fb8a4a212206374f5273bae
ximenazelaya1989 <20235973@esen.edu.sv>
2025-03-22 10:39:36 -0600

    correciones de rutas de user y products

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::get('/users/{userId}', [UserController::class, '']);
-    Route::patch('/users/{userId}', [UserController::class, '']);
-    Route::post('/user', [UserController::class, '']);
-    Route::get('/products', [ProductController::class, '']);
-    Route::get('/products/{productId}', [ProductController::class, '']);
-    Route::put('/products/{productId}', [ProductController::class, '']);
-    Route::delete('/products/{productId}', [ProductController::class, '']);
+
+    Route::post('/user', [UserController::class, 'store']);
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{userId}', [UserController::class, 'show']);
+    Route::patch('/users/{userId}', [UserController::class, 'update']);
+    Route::delete('/users/{userId}', [UserController::class, 'destroy']);
+
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{productId}', [ProductController::class, 'show']);
+    Route::put('/products/{productId}', [ProductController::class, 'update']);
+    Route::delete('/products/{productId}', [ProductController::class, 'destroy']);
 });
 
 
