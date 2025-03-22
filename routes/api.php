85c96ac48bf8c9619f07ef3fe0a7fdbd789b2afe
ximenazelaya1989 <20235973@esen.edu.sv>
2025-03-22 10:48:28 -0600

    product y user controller :)

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::group(['prefix' => 'v1'], function () {
 
