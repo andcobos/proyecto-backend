e56fe7f927e1431cab8327efc8fc5d3d39c12ce5
andcobos <146792784+andcobos@users.noreply.github.com>
2025-04-03 15:00:11 -0600

    product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -1,17 +1,27 @@
 <?php
-
 namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\StockStatus;
+use App\Models\Seller;
+use App\Models\ProductCategory;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        if (
+            StockStatus::count() === 0 ||
+            Seller::count() === 0 ||
+            ProductCategory::count() === 0
+        ) {
+            $this->command->warn('StockStatus, Seller o ProductCategory vacíos. Creando datos de referencia...');
+
+            StockStatus::factory()->count(2)->create();
+            Seller::factory()->count(2)->create();
+            ProductCategory::factory()->count(2)->create();
+        }
+
+        Product::factory()->count(10)->create();
     }
 }
