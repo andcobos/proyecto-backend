63c1946c05a811cf939b8bae10fcb96a9951d6c0
ximenazelaya1989 <20235973@esen.edu.sv>
2025-04-03 15:22:52 -0600

    cambio de database seeder :)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            PermissionSeeder::class,
             RolSeeder::class,               
             OrderStatusSeeder::class,       
             PaymentMethodSeeder::class,     
